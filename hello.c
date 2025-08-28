b0a2bfb051c155e5d3ef993b8e64af6054c94763
Milkzyy <maddoxjamesallen@gmail.com>
2025-08-28 12:30:24 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Maddox Gingras
+ * Date: 2025/28/08
+ *
+ * a simple program that prints my name.
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    printf("Hello! My name is Maddox Gingras!\n");
+
+    return 0;
+}
